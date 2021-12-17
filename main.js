a49ced27cddf5b3124a509a8b66f244b4bdd4e1c
rociogpaissaud <83926715+rociogpaissaud@users.noreply.github.com>
2021-12-17 15:53:33 -0300

    Practica Jquery y más

--- main.js
+++ main.js
@@ -1,352 +1,164 @@
-//Clase 8
-alert("Hola Rodri! Clase 8");
+/* Mi objtivo final:
+Armar el sitio con bootstrap y css. 
+Cargar datos en las cards.
+Armar una función para poder sumar las selecciones.
+Una vez cargado lo seleccionado, mostrar la compra
+Confirmar la compra.
+Ir al formulario para cargar los datos.
+Enviar información y guardar en el session o local.*/
 
-let armarFooter = document.createElement ("footer");
-armarFooter.innerHTML = "<p>Hecho por Rocío González Paissaud</p>";
-document.body.appendChild(armarFooter);
+//Cargar Información
 
-
-//Clase 6, 7 Y 8
-
-class Medallon {
-    constructor(id, tipoMedallon, precioMedallon) {
-    this.id = id;
-    this.tipoMedallon = tipoMedallon.toLowerCase();
-    this.precioMedallon = precioMedallon;
-    }
-    sumaIva() {
-      this.precioMedallon = this.precioMedallon * 1.21;
+class productosSandwich {
+    constructor (nombre, imagen, precio){
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.precio = precio;
     }
 }
 
-const ingresoMedallones = [];
+const productosVeggiewich = [
+        {nombre: "Falafe al morrón", 
+        imagen: "../media/humus, tomte, morron y falafel.jpg", 
+        precio: 450,
+        id:1 }, 
 
-function cargarMedallones() {
-    var cargaDeMedallones = JSON.parse(sessionStorage.getItem('productos'));
+        {nombre: "Garbanzo con lechuga y salsa de tomate", 
+        imagen: "../media/Garbanzos y lechuga.jpg", 
+        precio: 380,
+        id:2 }, 
+        
+        {nombre: "Calabaza con rúcula", 
+        imagen: "../media/rucula, cebolla y calabaza.jpg", 
+        precio: 400,
+        id:3 }
+    ];
 
-    if (cargaDeMedallones == null || cargaDeMedallones.length == 0) {
-    const cant_medallones = parseInt(prompt('¿Cuántos medallones querés ingresar?'));
 
-    for (let i = 1; i <= cant_medallones; i++) {
-        var cargarProductoM = new Medallon(i, prompt('Ingrese los medallones'), parseInt(prompt('Ingrese el precio')));
-        cargarProductoM.sumaIva();
-        ingresoMedallones.push(cargarProductoM);}
+var contenido = "";
 
-        sessionStorage.setItem('productos', JSON.stringify(ingresoMedallones));
-    var cargaDeMedallones = JSON.parse(sessionStorage.getItem('productos'));
+
+
+for (let productos of productosVeggiewich){ 
+    contenido += "<div class= 'col-3' id='columna__producto'>";
+    contenido += "<div class='card' style='width: 18rem;'>";
+    contenido += "<img src= " + productos.imagen + "' class='card-img-top' alt='" + productos.nombre + "'>";
+    contenido += "<div class='card-body'>";
+    contenido += "<h5 class='card-title'>" + productos.nombre + "</h5>";
+    contenido += "<p class='card-text'> <strong> $" + productos.precio + "</p>";
+    contenido += "<button class='btn btn-primary' id='btn" + productos.id +"'>" + 'Seleccionar' + "</button>";
+    contenido += "</div>";
+    contenido += "</div>";
+    contenido += "</div>";
+
+}
+
+var contenedorProductos = document.getElementById("contenedor_productos");
+contenedorProductos.innerHTML = contenido;
+
+productosVeggiewich.forEach((producto) => {
+    let boton = document.getElementById("btn" + producto.id);
+    boton.addEventListener("click", respuestaSeleccion);
+    console.log ("Selección de producto");
+  });
+
+  
+let guardarSeleccion = [];
+
+function respuestaSeleccion(e) {
+    let seleccionado =  productosVeggiewich.find(element => `btn${element.id}` == e.target.id); 
+    console.log(seleccionado);
+}
+
+//Formulario y JSON
+
+let boton = document.getElementById("btnEnviar");
+boton.addEventListener("click", respuestaClick);
+
+boton.onmousemove = () => { console.log ("Deslicé el mouse por arriba")};
+
+let guardarDatos = [];
+
+function respuestaClick(e) {
+    e.preventDefault();
+    var cargaDeDatos = JSON.parse(sessionStorage.getItem('resultados'));
+
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let telefono = document.getElementById("telefono").value;
+    let consulta = document.getElementById("consulta").value;
+
+   //document.getElementById("resultado").innerHTML = "<p>Tu nombre es: " + nombre + "</p> " + "<p>Tu email es: " + email + "</p> " + "<p>Tu número de teléfono es: " + telefono + "</p>" + "<p>Tu consulta es: " + consulta + "</p>";
+
+    for (let formulario of guardarDatos){
+        guardarDatos.push(document.getElementById("resultado"))
+        sessionStorage.setItem('resultados', JSON.stringify(guardarDatos));
     }
 
-    console.log('Total de productos: ' + cargaDeMedallones.length);
+    console.log(guardarDatos);
 }
+console.log("Respuesta enviada");
 
-cargarMedallones();
-console.log('Los productos disponibles son: ' + sessionStorage.getItem('productos'));
 
-for (var precios of ingresoMedallones) {
-    console.log(ingresoMedallones.precioMedallon * 1.21);
-  }
-console.log(ingresoMedallones);
 
+//Agregué el footer con JQuery
 
-for (var medallon of ingresoMedallones) {
-    let contenedor =document.createElement ("div");
-    contenedor.innerHTML = `<h3> ID: ${medallon.id}</h3>
-                            <h4> ID: ${medallon.tipoMedallon}</h4>
-                            <p> ID: ${medallon.precioMedallon}</p>`;
+$('footer').append(`<div class="backFooter">
+            <a href="https://www.linkedin.com/in/roc%C3%ADo-gonz%C3%A1lez-paissaud/">LinkedIn</a>
+            <a href="https://github.com/rociogpaissaud">GitHub</a>
+            <a href="https://www.instagram.com/rocio.paissaud/">Instagram</a>
+            <p>Hecho por Rocío González Paissaud</p>
+            </div>`);
 
-    document.body.appendChild(contenedor);
-  }
-console.log(ingresoMedallones);
+$(".backFooter").css ( {
+        "background-color": "#fdf6eb",
+        "display": "flex",
+        "flex-wrap": "nowrap",
+        "padding": "50px",
+        "justify-content": "center",
+        "align-content":"space-between",
+})
 
 
 
+$("footer").prepend(`<div id="resultadoNewsletter" style="display:none">
+                    <form id="newsletter">
+                        <label for="formGroupExampleInput" class="form-label">¿Querés recibir el Newsletter?</label>
+                        <input id='nombreApellido' type="text" class="form-control" placeholder="Escribí tu nombre y apellido">
+                        <input id='mail' type="mail" class="form-control" placeholder="Escribí tu email">
+                        <input id='enviarNewsletter' type="button" class="form-control" value='Quiero recibir el Newletter'>
+                        <input id='noNewsletter' type="button" class="form-control" value='No, gracias'>
+                    </form> 
+                    </div>`);
 
-/*
-//CONDIMENTOS
-class Condimentos {
-    constructor (id, tipoCondimentos, precioCondimentos) {
-        this.id = id;
-        this.tipoCondimentos = tipoCondimentos.toLowerCase ();
-        this.precioCondimentos = precioCondimentos;
-    }
-}
+$("#resultadoNewsletter").fadeIn(500);
 
-const igresoCondimentos = [];
+$("#enviarNewsletter").click(function(e) {
+    e.preventDefault();
+    console.log($("#nombreApellido"). val());
+    console.log($("#mail"). val());
+    alert("Tus datos fueron enviado correctamente");
+    $("#newsletter").submit();
+});
 
-function cargarCondimentos() {
-    const cant_condimentos = parseInt(prompt("¿Cuántos condimentos querés ingresar?"));
+$("#resultadoNewsletter").css ( {
+    "margin": "100px",
+    "padding": "10px",
+    "justify-content": "center",
+    "align-content":"space-between",
+})
 
-    for (let i = 1; i <=cant_condimentos; i++) {
-        var cargarProductoC = new Condimentos (i, prompt("Ingrese los condimentos"), parseInt(prompt("Ingrese el precio")));
-        igresoCondimentos.push(cargarProductoC);
-    }
 
-    localStorage.setItem("productos", JSON.stringify(igresoCondimentos));
+//Animación con JQUERY del H1 en el inicio
+$(".animacionTituloJs").css({
+    "font-weight": "bold",
+    
+})
 
-}
-cargarCondimentos();
 
 
-------------------------------------------------------------------------
-//PAN Y SALSAS
-/*
-function productos_Veggiewich () { 
 
 
-    class Pan {
-        constructor (tipoPan, precioPan) {
-            this.tipoPan = prompt("Ingrese el tipo de pan").toLowerCase ();
-            this.precioPan = parseInt(prompt("Ingrese el precio"));
-        }
-    }
 
-    class Salsa {
-        constructor (tipoSalsa, precioSalsa) {
-            this.tipoSalsa = prompt("Ingrese el tipo de salsa").toLowerCase ();
-            this.precioSalsa = parseInt(prompt("Ingrese el precio"));
-        }
-    }
 
-    class Queso {
-        constructor (tipoQueso, precioQueso) {
-            this.tipoSalsa = prompt("Ingrese el tipo de Queso").toLowerCase ();
-            this.precioQueso = parseInt(prompt("Ingrese el precio"));
-        }
-    }
-
-    const ElegirMedallon = new Medallon ();
-    const ElegirCondimentos = new Condimentos ();
-    const ElegirPan = new Pan ();
-    const ElegirSalsa = new Salsa ();
-    const ElegirQueso = new Queso ();
-
-    console.log (ElegirMedallon, ElegirCondimentos, ElegirPan, ElegirSalsa, ElegirQueso);
-
-}
-
-productos_Veggiewich();
-
-*/
-
-/*
-//COMPLEMENTARIO 6
-alert("Hola Rodri! Desafío Complementario Clase 6");
-const sandwich = [];
-
-do{
-   let datosCliente1 = prompt("¿Cuál es tu Sandwich preferido? Ingresá los condimentos");
-   sandwich.push(datosCliente1.toUpperCase());
-   console.log(sandwich.string);
-
-   var vacio = datosCliente1 == ''
-
-}while(!(vacio));
-
-console.table (sandwich.sort());
-*/
-
-
-
-/*
-//Clase 5
-alert("Hola Rodri! Desafío Clase 5");
-
-function pedidoVeggiewich (){
-
-let elegirmedallon= prompt("Elija el medallón: lenteja, garbanzos o porotos");
-let elegirPan= prompt("Elija el pan: tradicional, salvado o centeno");
-let elegirBebida= prompt("Elija la bebida: Coca, Sprite o Agua sin gas");
-
-class Medallon {  
-    constructor (tipoDeMedallon, tipoDePan, tipoDeBebida) {
-        this.tipoDeMedallon = tipoDeMedallon.toLowerCase ();
-        this.tipoDePan = tipoDePan.toLowerCase ();
-        this.tipoDeBebida = tipoDeBebida.toLowerCase ();
-        }
-}
-
-const eleccionMedallon1 = new Medallon (elegirmedallon, elegirPan, elegirBebida);
-console.log (eleccionMedallon1);
-}
-
-pedidoVeggiewich ();
-pedidoVeggiewich ();*/
-
-
-/*
-//Clase 4
-alert("Hola Rodri! Desafío Clase 4 y complementario");
-
-function veggiwich () {
-    let medallonLenteja = 200;
-    let pan = 50;
-    let tomate = 100;
-    let preguntaTomate = prompt("¿Querés tomate?");
-    let producto = "";
-
-    if ((preguntaTomate == "si")) {
-        producto = medallonLenteja + pan + tomate;
-    } else {
-        producto = medallonLenteja + pan;
-    }
-
-    return producto;
-}
-
-
-function veggiwichConBebida (producto){
-    let bebida = 80;
-    let preguntaBebida = prompt("¿Agregamos una bebida?");
-    if ((preguntaBebida == "si")) {
-        producto += bebida;
-    } 
-    return producto;
-}
-
-function veggiwichFinal (producto) {
-    alert("La hamburguesa costará: $ " + producto);
-}
-
-veggiwichFinal(veggiwichConBebida(veggiwich()));
-
-
-function solicitarBebidas (){ 
-    let bebidas = prompt("Elegí tu bebida: Coca, Sprite o Agua");
-        while (bebidas != ""){
-            switch (bebidas) {
-                case "Coca": 
-                    alert ("Usted solicito Coca");
-                    break;
-                case "Sprite": 
-                    alert ("Usted solicito Sprite");
-                    break;
-                case "Agua": 
-                    alert ("Usted solicito Agua");
-                    break;
-                default: 
-                    alert ("Usted no solicito las bebidas ofrecidas");
-                    break;
-        }
-            bebidas = prompt("Elegí tu bebida: Coca, Sprite o Agua");
-        }
-
-}
-
-solicitarBebidas();
-
-
-//Clase 3
-/*
-alert("Hola Rodri! Desafío Clase 3");
-
-let maximoMedallon = 5;
-let medallones = 0;
-
-for (let i=0; i <=10; i++) {
-    let pregunta = parseInt(prompt("¿Cuántos medallones de porotos quiere?"));
-    medallones = medallones + pregunta;
-
-    if (medallones > maximoMedallon) {
-
-        break;
-    }
-}
-console.log ("No hay más medallones de porotos disponibles");
-
-
-let bebidas = prompt("Elegí tu bebida: Coca, Sprite o Agua");
-while (bebidas != "ESC"){
-    switch (bebidas) {
-        case "Coca": 
-            alert ("Usted solicito Coca");
-            break;
-        case "Sprite": 
-            alert ("Usted solicito Sprite");
-            break;
-        case "Agua": 
-            alert ("Usted solicito Agua");
-            break;
-        default: 
-            alert ("Usted no solicito las bebidas ofrecidas");
-            break;
-}
-    bebidas = prompt("Elegí tu bebida: Coca, Sprite o Agua");
-}
-
-function armarHamburguesa (producto, bebida, operacion){
-    switch(operacion){
-        case "+":
-            return producto + bebida;
-            break;
-        case "-":
-            return producto + bebida;
-            break;
-    }
-
-}
-alert(armarHamburguesa(producto, 150, "+"));
-/*alert(armarHamburguesa(200, 50, "-"));
-
-*/
-
-/*
-//Clase 2
-
-alert("Hola Rodri! Desafío Clase 2");
-
-let mayorEdad = 18;
-let ingresaEdad = parseInt(prompt("Ingrese su edad"));
-
-if (ingresaEdad >= mayorEdad)  {
-    alert("Podés ingresar a la plataforma");
-    let ingreseNombre = prompt("Ingrese su nombre");
-    alert("El usuario ingresado es " + ingreseNombre);
-} else  {
-    alert("No podés ingresar a la plataforma");
-}
-
-
-
-let gustoFavorito = prompt("¿Cuál es tu gusto preferido de helado?");
-
-if (gustoFavorito != "tramontana") {
-    alert("Tu gusto preferido es " + gustoFavorito);
-} else {
-    alert("¡El mio también!");
-}
-
-*/
-
-/*
-//Clase 1
-
-alert("Hola Rodrigo! Hice varias pruebas");
-
-var numeroA = parseInt(prompt("Ingresá tu número favorito"));
-let numeroB = parseInt(prompt("Ingresá tu edad"));
-const numeroC = 4;
-
-let resultadoSuma = numeroA + numeroB;
-alert("El resultado de la suma es: " + resultadoSuma);
-
-let resultadoResta = numeroA - numeroC;
-console.log ("El kiosquero debe devolverme $" + resultadoResta);
-
-let resultadoMultiplicacion = numeroA * numeroC;
-alert("Cada manzana cuesta $ " + resultadoMultiplicacion);
-
-
-var textoA = "CURSANDO";
-var textoB = " JS";
-var textoC = "TENGO "
-
-let resultadoTexto = textoA + textoB;
-console.log ("Estoy " + resultadoTexto);
-
-let resultadoNumeroTexto = textoC + numeroC;
-alert(resultadoNumeroTexto + " hermanos");
-
-
-let gustoFavorito = prompt("¿Cuál es tu gusto preferido de helado?");
-alert("Mi helado favorito es " + gustoFavorito);*/
 
